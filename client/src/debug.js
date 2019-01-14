d2b3d0d004c256041495c0b4d2014b3e74a101b2
GuRu <gulyaev@tiburon-research.ru>
2019-01-14 13:39:27 +0300

    это не надо

--- client/src/debug.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var vscode = require("vscode");
-function test() {
-    vscode.window.activeTextEditor.edit(function (builder) {
-        builder.setEndOfLine(vscode.EndOfLine.LF);
-    });
-}
-exports.test = test;
