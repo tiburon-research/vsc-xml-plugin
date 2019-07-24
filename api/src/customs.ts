1ab366d0a96ba554835d70b7063d86c518ee2c63
GuRu <gulyaev@tiburon-research.ru>
2019-07-24 11:24:04 +0300

    берём другой xlsx parser

--- api/src/customs.ts
+++ api/src/customs.ts
@@ -14,433 +14,444 @@ import * as process from 'child_process'
 /** проверяет наличие файла/папки */
 export function pathExists(path: string): boolean
 {
-    return fs.existsSync(path);
+	return fs.existsSync(path);
 }
 
 /** создаёт папку */
 export function createDir(path: string, createWholePath = false)
 {
-    if (createWholePath)
-    {
-        let delimiter = '\\';
-        if (path.indexOf(delimiter) < 0) delimiter = '/';
-        let folders = path.split(delimiter);
-        if (folders.length == 0) throw "Не удалось распознать путь";
-        let prevPath = '';
-        folders.forEach(f =>
-        {
-            prevPath += f + delimiter;
-            if (!pathExists(prevPath)) fs.mkdirSync(prevPath);
-        });
-    }
-    else fs.mkdirSync(path);
+	if (createWholePath)
+	{
+	    let delimiter = '\\';
+	    if (path.indexOf(delimiter) < 0) delimiter = '/';
+	    let folders = path.split(delimiter);
+	    if (folders.length == 0) throw "Не удалось распознать путь";
+	    let prevPath = '';
+	    folders.forEach(f =>
+	    {
+	        prevPath += f + delimiter;
+	        if (!pathExists(prevPath)) fs.mkdirSync(prevPath);
+	    });
+	}
+	else fs.mkdirSync(path);
 }
 
 /** Преобразует путь в URI */
 export function uriFromName(path: string): string
 {
-    return Uri.file(path).toString()
+	return Uri.file(path).toString()
 }
 
 
 /** Задаёт файлу режим readonly */
 export function unlockFile(path: string)
 {
-    //winattr.setSync(path, { readonly: false });
-    //fs.chmodSync(path, '666');
-    process.execSync(`attrib -r "${path}"`);
+	//winattr.setSync(path, { readonly: false });
+	//fs.chmodSync(path, '666');
+	process.execSync(`attrib -r "${path}"`);
 }
 
 
 /** Снимает с файла режим readonly */
 export function lockFile(path: string)
 {
-    if (!pathExists(path)) return;
-    //winattr.setSync(path, { readonly: true });
-    //fs.chmodSync(path, '444');
-    process.execSync(`attrib +r "${path}"`);
+	if (!pathExists(path)) return;
+	//winattr.setSync(path, { readonly: true });
+	//fs.chmodSync(path, '444');
+	process.execSync(`attrib +r "${path}"`);
 }
 
 /** Файл в режиме readonly */
 export function fileIsLocked(path: string): boolean
 {
-    if (!pathExists(path)) return false;
-    //let props = winattr.getSync(path);
-    let a = fs.statSync(path);
-    let readOnly = (a.mode & parseInt('777', 8)).toString(8) == '444';
-    return readOnly;
-    //return !!props && !!props.readonly;
+	if (!pathExists(path)) return false;
+	//let props = winattr.getSync(path);
+	let a = fs.statSync(path);
+	let readOnly = (a.mode & parseInt('777', 8)).toString(8) == '444';
+	return readOnly;
+	//return !!props && !!props.readonly;
 }
 
 
 /** Делает файл hidden */
 export function hideFile(path: string)
 {
-    process.execSync(`attrib +h "${path}"`);
-    //winattr.setSync(path, { hidden: true });
+	process.execSync(`attrib +h "${path}"`);
+	//winattr.setSync(path, { hidden: true });
 }
 
 /** Снимает свойство hidden с файла */
 export function showFile(path: string)
 {
-    process.execSync(`attrib -h "${path}"`);
-    //winattr.setSync(path, { hidden: false });
+	process.execSync(`attrib -h "${path}"`);
+	//winattr.setSync(path, { hidden: false });
 }
 
 
 /** возвращает минимальное неотрицательное или `negative` (= null), если нет таких */
 export function positiveMin(a, b, negative: any = null)
 {
-    let neg = null;
-    if (typeof negative !== typeof null) neg = negative;
+	let neg = null;
+	if (typeof negative !== typeof null) neg = negative;
 
-    if (a < 0)
-        if (b < 0) return neg;
-        else return b;
-    else
-        if (b < 0) return a;
-        else return Math.min(a, b);
+	if (a < 0)
+	    if (b < 0) return neg;
+	    else return b;
+	else
+	    if (b < 0) return a;
+	    else return Math.min(a, b);
 }
 
 
 /** записывает данные в буфер обмена */
 export function copyToClipboard(text: string)
 {
-    clipboard.writeSync(text);
+	clipboard.writeSync(text);
 }
 
 
 /** получает данные из буфера обмена */
 export function getFromClioboard(): string
 {
-    return clipboard.readSync();
+	return clipboard.readSync();
 }
 
 
 /** Подготовленная для RegExp строка */
 export function safeString(text: string): string
 {
-    if (!text) return '';
-    return text.replace(/[\|\\\{\}\(\)\[\]\^\$\+\*\?\.\/]/g, "\\$&");
+	if (!text) return '';
+	return text.replace(/[\|\\\{\}\(\)\[\]\^\$\+\*\?\.\/]/g, "\\$&");
 }
 
 
 /** Пара ключ-значение */
 export interface IPair<T>
 {
-    Key: string;
-    Value: T;
+	Key: string;
+	Value: T;
 }
 
 
 /** Элемент `KeyedCollection` */
 export class KeyValuePair<T> implements IPair<T>
 {
-    constructor(key: string, value: T)
-    {
-        this.Key = key;
-        this.Value = value;
-    }
+	constructor(key: string, value: T)
+	{
+	    this.Key = key;
+	    this.Value = value;
+	}
 
-    Key: string;
-    Value: T;
+	Key: string;
+	Value: T;
 }
 
 
 export class KeyedCollection<T>
 {
-    protected items: { [index: string]: T } = {};
-    private count: number = 0;
+	protected items: { [index: string]: T } = {};
+	private count: number = 0;
 
-    constructor()
-    {
-    }
+	constructor()
+	{
+	}
 
-    public ToSimpleObject(): Object
-    {
-        let res = {};
-        this.ForEach((key, value) => { res[key] = value });
-        return res;
-    }
+	public ToSimpleObject(): Object
+	{
+	    let res = {};
+	    this.ForEach((key, value) => { res[key] = value });
+	    return res;
+	}
 
-    public static FromObject(obj: Object): KeyedCollection<any>
-    {
-        let res = new KeyedCollection<any>();
-        for (let key in obj) {
-            if (obj.hasOwnProperty(key)) res.AddPair(key, obj[key]);
-        }
-        return res;
-    }
+	public static FromObject(obj: Object): KeyedCollection<any>
+	{
+	    let res = new KeyedCollection<any>();
+	    for (let key in obj) {
+	        if (obj.hasOwnProperty(key)) res.AddPair(key, obj[key]);
+	    }
+	    return res;
+	}
 
-    /** Создаёт коллекцию из массивов ключей и значений */
-    public static FromArrays<T>(keys: string[], values: T[]): KeyedCollection<T>
-    {
-        if (keys.length != values.length) throw "Количества ключей и значений отличаются";
-        let res = new KeyedCollection<T>();
-        for (let i = 0; i < keys.length; i++)
-        {
-            res.AddPair(keys[i], values[i]);
-        }
-        return res;
-    }
+	/** Создаёт коллекцию из массивов ключей и значений */
+	public static FromArrays<T>(keys: string[], values: T[]): KeyedCollection<T>
+	{
+	    if (keys.length != values.length) throw "Количества ключей и значений отличаются";
+	    let res = new KeyedCollection<T>();
+	    for (let i = 0; i < keys.length; i++)
+	    {
+	        res.AddPair(keys[i], values[i]);
+	    }
+	    return res;
+	}
 
-    /** Создаёт коллекцию из массива `IPair` */
-    public static FromPairs<T>(pairs: IPair<T>[]): KeyedCollection<T>
-    {
-        let res = new KeyedCollection<T>();
-        pairs.forEach(pair =>
-        {
-            res.AddPair(pair.Key, pair.Value);
-        });
-        return res;
-    }
+	/** Создаёт коллекцию из массива `IPair` */
+	public static FromPairs<T>(pairs: IPair<T>[]): KeyedCollection<T>
+	{
+	    let res = new KeyedCollection<T>();
+	    pairs.forEach(pair =>
+	    {
+	        res.AddPair(pair.Key, pair.Value);
+	    });
+	    return res;
+	}
 
-    /** Проверяет наличие ключа */
-    public ContainsKey(key: string): boolean
-    {
-        return this.items.hasOwnProperty(key);
-    }
+	/** Проверка поэлементно */
+	public Find(filter: (key: string, val: T) => boolean): KeyValuePair<T>
+	{
+	    let res: KeyValuePair<T>;
+	    this.ForEach((key, value) =>
+	    {
+	        if (filter(key, value)) return res = new KeyValuePair(key, value);
+	    });
+	    return res;
+	}
 
-    public Count(): number
-    {
-        return this.count;
-    }
+	/** Проверяет наличие ключа */
+	public ContainsKey(key: string): boolean
+	{
+	    return this.items.hasOwnProperty(key);
+	}
 
-    public AddElement(element: KeyValuePair<T>)
-    {
-        this.AddPair(element.Key, element.Value);
-    }
+	public Count(): number
+	{
+	    return this.count;
+	}
 
-    /** Добавляет или заменяет */
-    public AddPair(key: string, value: T)
-    {
-        if (!this.items.hasOwnProperty(key))
-            this.count++;
+	public AddElement(element: KeyValuePair<T>)
+	{
+	    this.AddPair(element.Key, element.Value);
+	}
 
-        this.items[key] = value;
-    }
+	/** Добавляет или заменяет */
+	public AddPair(key: string, value: T)
+	{
+	    if (!this.items.hasOwnProperty(key))
+	        this.count++;
 
-    public Remove(key: string): T
-    {
-        let val = this.items[key];
-        delete this.items[key];
-        this.count--;
-        return val;
-    }
+	    this.items[key] = value;
+	}
 
-    public Item(key: string): T
-    {
-        return this.items[key];
-    }
+	public Remove(key: string): T
+	{
+	    let val = this.items[key];
+	    delete this.items[key];
+	    this.count--;
+	    return val;
+	}
 
-    /** массив ключей */
-    public Keys(): string[]
-    {
-        let keySet: string[] = [];
+	public Item(key: string): T
+	{
+	    return this.items[key];
+	}
 
-        for (let prop in this.items)
-        {
-            if (this.items.hasOwnProperty(prop))
-            {
-                keySet.push(prop);
-            }
-        }
+	/** массив ключей */
+	public Keys(): string[]
+	{
+	    let keySet: string[] = [];
 
-        return keySet;
-    }
+	    for (let prop in this.items)
+	    {
+	        if (this.items.hasOwnProperty(prop))
+	        {
+	            keySet.push(prop);
+	        }
+	    }
 
-    /** массив значений */
-    public Values(): T[]
-    {
-        let values: T[] = [];
+	    return keySet;
+	}
 
-        for (let prop in this.items)
-        {
-            if (this.items.hasOwnProperty(prop))
-            {
-                values.push(this.items[prop]);
-            }
-        }
+	/** массив значений */
+	public Values(): T[]
+	{
+	    let values: T[] = [];
 
-        return values;
-    }
+	    for (let prop in this.items)
+	    {
+	        if (this.items.hasOwnProperty(prop))
+	        {
+	            values.push(this.items[prop]);
+	        }
+	    }
 
-    /** Очищает всю коллекцию */
-    public Clear(): void
-    {
-        this.items = {};
-        this.count = 0;
-    }
+	    return values;
+	}
 
-    /** обход элементов */
-    public ForEach(callback: (key: string, val: T) => any)
-    {
-        for (let key in this.items)
-            callback(key, this.Item(key));
-    }
+	/** Очищает всю коллекцию */
+	public Clear(): void
+	{
+	    this.items = {};
+	    this.count = 0;
+	}
+
+	/** обход элементов */
+	public ForEach(callback: (key: string, val: T) => any)
+	{
+	    for (let key in this.items)
+	        callback(key, this.Item(key));
+	}
 
 	/** 
 	 * преобразует набор 
 	 * @param clearNull очищать ли по проверке (!!element)
 	*/
-    public Select(filter: (key: string, value: T) => any, clearNull = false): any[]
-    {
-        let res = [];
-        this.ForEach((key, value) =>
-        {
-            let item = filter(key, value);
-            if (!clearNull || !!item) res.push(item);
-        });
-        return res;
-    }
+	public Select(filter: (key: string, value: T) => any, clearNull = false): any[]
+	{
+	    let res = [];
+	    this.ForEach((key, value) =>
+	    {
+	        let item = filter(key, value);
+	        if (!clearNull || !!item) res.push(item);
+	    });
+	    return res;
+	}
 
-    /** Фильтрует набор */
-    protected Filter(filter: (key: string, value: T) => boolean): KeyedCollection<T>
-    {
-        let res = new KeyedCollection<T>();
-        this.ForEach((key, value) =>
-        {
-            if (filter(key, value)) res.AddPair(key, value);
-        });
-        return res;
-    }
+	/** Фильтрует набор */
+	protected Filter(filter: (key: string, value: T) => boolean): KeyedCollection<T>
+	{
+	    let res = new KeyedCollection<T>();
+	    this.ForEach((key, value) =>
+	    {
+	        if (filter(key, value)) res.AddPair(key, value);
+	    });
+	    return res;
+	}
 
-    /** Обновляет значение элемента по ключу */
-    public UpdateValue(key: string, transform: (value: T) => T): void
-    {
-        this.AddPair(key, transform(this.Item(key)));
-    }
+	/** Обновляет значение элемента по ключу */
+	public UpdateValue(key: string, transform: (value: T) => T): void
+	{
+	    this.AddPair(key, transform(this.Item(key)));
+	}
 
-    /** Добавляет диапазон значений */
-    public AddRange(range: KeyedCollection<T>): void
-    {
-        range.ForEach((key, value) =>
-        {
-            this.AddPair(key, value);
-        })
-    }
+	/** Добавляет диапазон значений */
+	public AddRange(range: KeyedCollection<T>): void
+	{
+	    range.ForEach((key, value) =>
+	    {
+	        this.AddPair(key, value);
+	    })
+	}
 
-    /** Преобразует коллекцию в новую */
-    public Map(func: (key: string, value: T) => KeyValuePair<any>): KeyedCollection<T>
-    {
-        let res = new KeyedCollection<T>();
-        this.ForEach((key, value) =>
-        {
-            res.AddElement(func(key, value));
-        });
-        return res;
-    }
+	/** Преобразует коллекцию в новую */
+	public Map(func: (key: string, value: T) => KeyValuePair<any>): KeyedCollection<T>
+	{
+	    let res = new KeyedCollection<T>();
+	    this.ForEach((key, value) =>
+	    {
+	        res.AddElement(func(key, value));
+	    });
+	    return res;
+	}
 
-    /** Преобразует коллекцию в массив */
-    public ToArray(func: (element: KeyValuePair<T>) => any): any[]
-    {
-        let ar: KeyValuePair<T>[] = [];
-        this.ForEach((key, value) => { ar.push(new KeyValuePair(key, value)); });
-        return ar.map(func);
-    }
+	/** Преобразует коллекцию в массив */
+	public ToArray(func: (element: KeyValuePair<T>) => any): any[]
+	{
+	    let ar: KeyValuePair<T>[] = [];
+	    this.ForEach((key, value) => { ar.push(new KeyValuePair(key, value)); });
+	    return ar.map(func);
+	}
 
 
-    /** Возвращает отсортированную массив пар */
-    public OrderBy(func: (x: KeyValuePair<T>) => number): KeyValuePair<T>[]
-    {
-        let res: KeyValuePair<T>[] = [];
-        let sortedAr: KeyValuePair<T>[] = this.ToArray(x => x);
-        sortedAr = sortedAr.sort(x => func(x));
-        sortedAr.forEach(element =>
-        {
-            res.push(element);
-        });
-        return res;
-    }
+	/** Возвращает отсортированную массив пар */
+	public OrderBy(func: (x: KeyValuePair<T>) => number): KeyValuePair<T>[]
+	{
+	    let res: KeyValuePair<T>[] = [];
+	    let sortedAr: KeyValuePair<T>[] = this.ToArray(x => x);
+	    sortedAr = sortedAr.sort(x => func(x));
+	    sortedAr.forEach(element =>
+	    {
+	        res.push(element);
+	    });
+	    return res;
+	}
 
 }
 
 
 export class OrderedCollection<T>
 {
-    private items: KeyValuePair<T>[] = [];
-    private keys: string[] = [];
+	private items: KeyValuePair<T>[] = [];
+	private keys: string[] = [];
 
 
-    constructor()
-    { }
+	constructor()
+	{ }
 
-    private _addKey(key: string)
-    {
-        this.keys.push(key);
-    }
+	private _addKey(key: string)
+	{
+	    this.keys.push(key);
+	}
 
-    protected _getIndex(key: string)
-    {
-        return this.keys.indexOf(key);
-    }
-
-
-
-    public Get(key: string): T
-    {
-        let ind = this._getIndex(key);
-        if (ind < 0) throw `Ключ "${key}" отсутствует в коллекции`;
-        return this.items[ind].Value;
-    }
+	protected _getIndex(key: string)
+	{
+	    return this.keys.indexOf(key);
+	}
 
 
-    public Add(key: string, item: T)
-    {
-        let ind = this._getIndex(key);
-        if (ind > -1) throw `Ключ ${key} уже присутствует в коллекции`;
-        this._addKey(key)
-        this.items.push(new KeyValuePair(key, item));
-    }
 
-    public get Count(): number
-    {
-        return this.items.length;
-    }
-
-    public Clear()
-    {
-        this.items = [];
-        this.keys = [];
-    }
-
-    public ForEach(callbackfn: (value: KeyValuePair<T>, index: number, array: KeyValuePair<T>[]) => void, thisArg?: any)
-    {
-        this.items.forEach(callbackfn, thisArg);
-    }
-
-    public Contains(key: string): boolean
-    {
-        return this._getIndex(key) > -1;
-    }
-
-    /** Обновляет старое значение `val` по ключу `key` */
-    public UpdateValue(key: string, func: (val: T) => T)
-    {
-        let ind = this._getIndex(key);
-        if (ind < 0) throw `Ключ "${key}" не найден в коллекции`;
-        this.items[ind].Value = func(this.items[ind].Value);
-    }
-
-    public Remove(key: string): T
-    {
-        let ind = this._getIndex(key);
-        if (ind < 0) return undefined;
-        this.keys.remove(key);
-        let val = this.items[ind].Value;
-        this.items = this.items.splice(ind, 1);
-        return val;
-    }
+	public Get(key: string): T
+	{
+	    let ind = this._getIndex(key);
+	    if (ind < 0) throw `Ключ "${key}" отсутствует в коллекции`;
+	    return this.items[ind].Value;
+	}
 
 
-    public Keys(): string[]
-    {
-        return this.keys;
-    }
+	public Add(key: string, item: T)
+	{
+	    let ind = this._getIndex(key);
+	    if (ind > -1) throw `Ключ ${key} уже присутствует в коллекции`;
+	    this._addKey(key)
+	    this.items.push(new KeyValuePair(key, item));
+	}
+
+	public get Count(): number
+	{
+	    return this.items.length;
+	}
+
+	public Clear()
+	{
+	    this.items = [];
+	    this.keys = [];
+	}
+
+	public ForEach(callbackfn: (value: KeyValuePair<T>, index: number, array: KeyValuePair<T>[]) => void, thisArg?: any)
+	{
+	    this.items.forEach(callbackfn, thisArg);
+	}
+
+	public Contains(key: string): boolean
+	{
+	    return this._getIndex(key) > -1;
+	}
+
+	/** Обновляет старое значение `val` по ключу `key` */
+	public UpdateValue(key: string, func: (val: T) => T)
+	{
+	    let ind = this._getIndex(key);
+	    if (ind < 0) throw `Ключ "${key}" не найден в коллекции`;
+	    this.items[ind].Value = func(this.items[ind].Value);
+	}
+
+	public Remove(key: string): T
+	{
+	    let ind = this._getIndex(key);
+	    if (ind < 0) return undefined;
+	    this.keys.remove(key);
+	    let val = this.items[ind].Value;
+	    this.items = this.items.splice(ind, 1);
+	    return val;
+	}
 
 
-    public ToArray(func: (T) => any): any[]
-    {
-        return this.items.map(x => func(x));
-    }
+	public Keys(): string[]
+	{
+	    return this.keys;
+	}
+
+
+	public ToArray(func: (T) => any): any[]
+	{
+	    return this.items.map(x => func(x));
+	}
 }
--- client/src/geo.ts
+++ client/src/geo.ts
@@ -1,161 +1,117 @@
-import * as xlsx from 'read-excel-file';
 import * as fs from 'fs';
-import { pathExists } from 'tib-api';
+import { pathExists, KeyedCollection } from 'tib-api';
 import { Path } from './classes';
+import xlsx from 'node-xlsx';
 
 
 const GeoPath = "T:\\=Tiburon_NEW\\Geo";
 
-function createInterface(prefix: string)
-{
-    let res = {};
-    let IdKey = prefix + "Id";
-    let NameKey = prefix + "Name";
-
-    res[prefix] = { prop: prefix, type: {} };
-    res[prefix]['type'][IdKey] = {
-        prop: 'Id',
-        type: Number
-    }
-    res[prefix]['type'][NameKey] = {
-        prop: 'Name',
-        type: String
-    }
-    return res;
-}
-
-
-let XlsxGeoSchecme = {
-    Country: createInterface('Country'),
-    City: createInterface('City'),
-    "CityPopulation": {
-        prop: 'CityPopulation',
-        type: Number
-    },
-    District: createInterface('District'),
-    Starta: createInterface('Starta'),
-    Subject: createInterface('Subject'),
-    FilMegafon: createInterface('FilMegafon')
-}
-
-/* let XlsxGeoSchecme = {
-
-    createInterface("Country"),
-    createInterface("City"),
-
-"CityId": {
-    prop: 'CityId',
-        type: Number
-},
-"CityName": {
-    prop: 'CityName',
-        type: String
-},
-"CityPopulation": {
-    prop: 'CityPopulation',
-        type: Number
-},
-"DistrictId": {
-    prop: 'DistrictId',
-        type: Number
-},
-"DistrictName": {
-    prop: 'DistrictName',
-        type: String
-},
-"StrataId": {
-    prop: 'StrataId',
-        type: Number
-},
-"StrataName": {
-    prop: 'StrataName',
-        type: String
-},
-"SubjectId": {
-    prop: 'SubjectId',
-        type: Number
-},
-"SubjectName": {
-    prop: 'SubjectName',
-        type: String
-},
-"FilMegafonId": {
-    prop: 'FilMegafonId',
-        type: Number
-},
-"FilMegafonName": {
-    prop: 'FilMegafonName',
-        type: String
-},
-}; */
-
-
 interface IdName
 {
-    Id: Number;
-    Name: String;
+	Id: Number;
+	Name: String;
 }
 
-interface GeoFileLineData
+class GeoFileLineData extends Object
 {
-    Country: IdName;
-    City: IdName;
-    CityPopulation: Number;
-    District: IdName;
-    Starta: IdName;
-    Subject: IdName;
-    FilMegafon: IdName;
+	CountryId: number;
+	CountryName: string;
+	CityId: number;
+	CityName: string;
+	CityPopulation: number;
+	DistrictId: number;
+	DistrictName: string;
+	StrataId: number;
+	StrataName: string;
+	SubjectId: number;
+	SubjectName: string;
+	FilMegafonId: number;
+	FilMegafonName: string;
 }
 
-export function getGeoPath(): string
+function getGeoPath(): string
 {
-    let res: string = null;
-    if (pathExists(GeoPath))
-    {
-        let files = fs.readdirSync(GeoPath).map(f =>
-        {
-            let res: number;
-            let match = f.match(/^(\d+)-geo\.xlsx$/);
-            if (!!match) res = Number(match[1]);
-            return res;
-        }).filter(f => !!f).sort((a, b) => { return b - a; });
-        if (files.length > 0) res = Path.Concat(GeoPath, files[0] + "-geo.xlsx");
-    }
-    return res;
+	let res: string = null;
+	if (pathExists(GeoPath))
+	{
+	    let files = fs.readdirSync(GeoPath).map(f =>
+	    {
+	        let res: number;
+	        let match = f.match(/^(\d+)-geo\.xlsx$/);
+	        if (!!match) res = Number(match[1]);
+	        return res;
+	    }).filter(f => !!f).sort((a, b) => { return b - a; });
+	    if (files.length > 0) res = Path.Concat(GeoPath, files[0] + "-geo.xlsx");
+	}
+	return res;
 }
 
-async function readGeoFileLines(path: string): Promise<GeoFileLineData[]>
+/** Все строки первого листа (Geo) */
+async function readGeoFileList(path: string): Promise<[][]>
 {
-    let res: GeoFileLineData[] = [];
-    let fileData: { rows: [], errors: [] } = await xlsx.readXlsxFile(path, { schema: XlsxGeoSchecme, sheet: 'Geo' });
-    if (!!fileData.errors && fileData.errors.length > 0) throw fileData.errors.join('\n');
-    return fileData.rows;
+	return xlsx.parse(path)[0].data;
 }
 
+async function parseGeoList(data: [][]): Promise<GeoFileData>
+{
+	let lines = data.concat([]);
+	let cellNames = lines.shift();
+	let res = new GeoFileData();
+	lines.forEach(line =>
+	{
+	    if (line.length != cellNames.length) return;
+	    let lineData = new GeoFileLineData();
+	    for (let i = 0; i < cellNames.length; i++)
+	    {
+	        let name = cellNames[i];
+	        if (lineData.hasOwnProperty(name)) lineData[name] = line[i];
+	    }
+	    res.AddLine(lineData);
+	});
+	return res;
+}
+
+interface CityData extends IdName
+{
+	Population: number;
+}
 
 class GeoFileData
 {
-    Geo: GeoFileLineData[];
+	Geo: GeoFileLineData[] = [];
+	City: CityData[];
+	Country: IdName[];
+	Subject: IdName[];
+	District: IdName[];
+    Starta: IdName[];
+    
+    private _propertyList = ["Country", "Subject", "District", "Strata"];
 
-    Countries: IdName[];
-    Subjects: IdName[];
-    Districts: IdName[];
+	public AddLine(data: GeoFileLineData)
+	{
+        this.Geo.push(data);
+        
+        if (!!data.CityId && !this.City.find(value => { value.Id == data.CityId })) this.City.push({ Id: data.CityId, Name: data.CityName, Population: data.CityPopulation });
+        
+        this._propertyList.forEach(prefix =>
+        {
+            let thisProp = this[prefix] as IdName[];
+            let dataPropId = data[prefix + "Id"] as number;
+            
+            if (!!dataPropId && !thisProp.find(x => { x.Id == dataPropId })) thisProp.push({ Id: dataPropId, Name:  data[prefix + "Name"] });
+        });
+	}
 }
 
 
-const Lists = [
-    "Countries",
-    "Subjects",
-    "Districts"
-];
-
-
-async function readGeoFile(): Promise<GeoFileData>
+export async function readGeoFile(): Promise<GeoFileData>
 {
-    let res = new GeoFileData();
-    let path = getGeoPath();
+	let path = getGeoPath();
     if (!path) throw "Путь к файлу с георафией не найден";
-
-    let lines = await readGeoFileLines(path);
-    return res;
+    let geoList = await readGeoFileList("T:\\=Tiburon_NEW\\Geo\\test.xlsx");
+    console.log('start');
+    let res = await parseGeoList(geoList);
+    console.log('stop');
+    console.log(res);
+	return res;
 }
