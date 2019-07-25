f839a8d89720e98832f725d67e483b75253b0d36
GuRu <gulyaev@tiburon-research.ru>
2019-07-25 18:14:56 +0300

    генерация странички

--- client/src/geo.ts
+++ client/src/geo.ts
@@ -13,6 +13,16 @@ export const GeoConstants = {
 	GroupBy: {
 		Subject: "Федеральный округ",
 		District: "Область"
+	},
+	ListNames: {
+		City: "cityList",
+		District: "districtList",
+		Subject: "subjectList"
+	},
+	QuestionNames: {
+		District: "District",
+		Subject: "Subject",
+		City: "City"
 	}
 }
 
@@ -100,7 +110,7 @@ export async function createGeolists(cities: GeoFileLineData[], groupBy: string[
 	// ФО
 	if (groupBy.contains(GeoConstants.GroupBy.District))
 	{
-		let districtList = new SurveyList("districtList");
+		let districtList = new SurveyList(GeoConstants.ListNames.District);
 		districtList.VarsAsTags = false;
 		let filteredDistricts = KeyedCollection.FromPairs(cities.map(x => { return { Key: x.DistrictId, Value: x.DistrictName } }));
 		filteredDistricts.ForEach((key, value) =>
@@ -113,7 +123,7 @@ export async function createGeolists(cities: GeoFileLineData[], groupBy: string[
 	// Области
 	if (groupBy.contains(GeoConstants.GroupBy.Subject))
 	{
-		let subjectList = new SurveyList("subjectList");
+		let subjectList = new SurveyList(GeoConstants.ListNames.Subject);
 		subjectList.VarsAsTags = false;
 		let filteredSubjects = KeyedCollection.FromPairs(cities.map(x => { return { Key: x.SubjectId, Value: x.SubjectName } }));
 		filteredSubjects.ForEach((key, value) =>
@@ -125,12 +135,20 @@ export async function createGeolists(cities: GeoFileLineData[], groupBy: string[
 	}
 
 	// города
-	let cityList = new SurveyList("cityList");
+	let cityList = new SurveyList(GeoConstants.ListNames.City);
 	cityList.VarsAsTags = false;
 	cities.forEach(city =>
 	{
 		cityList.AddItem({ Id: city.CityId, Text: city.CityName, Vars: [city.SubjectId, city.StrataId, city.DistrictId] });
 	});
+
+	res += `
+	<!--**
+		Var[0] - ФО (District/Регион)
+		Var[1] - Страта
+		Var[2] - Область (Subject)
+	-->
+	`;
 
 	res += cityList.ToXML() + '\n\n';
 
@@ -138,3 +156,76 @@ export async function createGeolists(cities: GeoFileLineData[], groupBy: string[
 }
 
 
+export async function ceateGeoPage(groupBy: string[]): Promise<string>
+{
+	let pageName = "Geo";
+	let res = `<Page Id="${pageName}">\n`;
+	let resetId = "999999";
+
+	// ФО (District)
+	if (groupBy.contains(GeoConstants.GroupBy.District))
+	{
+		res += `
+		<Question Id="${GeoConstants.QuestionNames.District}" Type="RadioButton">
+			<Header>В каком регионе Вы проживаете?</Header>
+			<Repeat List="${GeoConstants.ListNames.District}">
+				<Answer Id="@ID">
+					<Filter Side="Client">return AnswerExists("${GeoConstants.QuestionNames.District}","@ID") || AnswerExists("${GeoConstants.QuestionNames.District}","${resetId}") || AnswerCount("${pageName}","${GeoConstants.QuestionNames.District}") == 0;</Filter>
+					<Text>@Text</Text>
+				</Answer>
+			</Repeat>
+			<Answer Id="${resetId}">
+				<Filter Side="Client">return AnswerExistsAny("${GeoConstants.QuestionNames.District}","$repeat(${GeoConstants.ListNames.District}){@ID[,]}");</Filter>
+				<Ui Isolate="1"/>
+				<Text>Изменить регион</Text>
+			</Answer>
+		</Question>
+		`;
+	}
+
+	// Область (Subject)
+	if (groupBy.contains(GeoConstants.GroupBy.Subject))
+	{
+		let answerDistrictFilter = groupBy.contains(GeoConstants.GroupBy.District) ? `AnswerExists("${GeoConstants.QuestionNames.District}","@Var(0)") && ` : '';
+		res += `
+		<Question Id="${GeoConstants.QuestionNames.Subject}" Type="RadioButton">
+			<Header>В какой области Вы проживаете?</Header>
+			<Repeat List="${GeoConstants.ListNames.Subject}">
+				<Answer Id="@ID">
+					<Filter Side="Client">return ${answerDistrictFilter}(AnswerExists("${GeoConstants.QuestionNames.Subject}","@ID") || AnswerExists("${GeoConstants.QuestionNames.Subject}","${resetId}") || AnswerCount("${pageName}","${GeoConstants.QuestionNames.Subject}") == 0);</Filter>
+					<Text>@Text</Text>
+				</Answer>
+			</Repeat>
+			<Answer Id="${resetId}">
+				<Filter Side="Client">return AnswerExistsAny("${GeoConstants.QuestionNames.Subject}","$repeat(${GeoConstants.ListNames.Subject}){@ID[,]}");</Filter>
+				<Ui Isolate="1"/>
+				<Text>Изменить область</Text>
+			</Answer>
+		</Question>
+		`;
+	}
+
+	// Город
+	res += `
+	<Question Id="${GeoConstants.QuestionNames.City}" Type="RadioButton">
+		<Filter Side="Client">return AnswerExistsAny("${GeoConstants.QuestionNames.District}","$repeat(${GeoConstants.QuestionNames.City}){@ID[,]}");</Filter>
+		<Header>В каком городе Вы проживаете?</Header>
+		<Repeat List="${GeoConstants.ListNames.City}">
+			<Answer Id="@ID"><Text>@Text</Text><Filter Side="Client">@AnswerExists("${GeoConstants.QuestionNames.District}","@Var(0)");</Filter></Answer>
+		</Repeat>
+		<Answer Id="${resetId}"><Text>Другой</Text></Answer>
+	</Question>
+	`;
+
+	// Redirect
+	res += `
+	<Redirect Status="19">
+		string city = AnswerID("${GeoConstants.QuestionNames.City}");
+		if (city == "${resetId}") return true;
+		return false;
+	</Redirect>
+	`;
+		
+	res += "</Page>";
+	return res;
+}
